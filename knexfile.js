84c89d955f8767b96ab9bccccc3cdf8f47840293
Your Name <you@example.com>
2020-03-18 19:44:48 -0700

    removed production

--- knexfile.js
+++ knexfile.js
@@ -24,22 +24,22 @@ module.exports = {
     migrations: {
       tableName: './migrations'
     }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 
 };
