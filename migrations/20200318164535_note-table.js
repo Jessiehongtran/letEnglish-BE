ac4360de89d77bcb8379939edddd47a67c65ebb8
Your Name <you@example.com>
2020-03-27 15:49:33 -0700

    created new database to increase length of string

--- migrations/20200318164535_note-table.js
+++ migrations/20200318164535_note-table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('note', tbl => {
         tbl.increments();
-        tbl.string('content');
+        tbl.string('content', 5000);
         tbl.string('created_at')
     })
   
